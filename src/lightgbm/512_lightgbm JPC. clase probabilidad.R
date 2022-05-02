fc58c1de7cb5a97a91807565a5fe1a07576ac2cc
cadaveirajp <101825524+cadaveirajp@users.noreply.github.com>
2022-05-02 17:51:52 -0400

    Clase del sábado 4/30
    Clase del sábado 4/30

--- src/lightgbm/512_lightgbm JPC. clase probabilidad.R
+++ src/lightgbm/512_lightgbm JPC. clase probabilidad.R
@@ -1,11 +1,11 @@
-# XGBoost  sabor tradicional ,  cambiando algunos de los parametros
+# LightGBM  cambiando algunos de los parametros
 
 #limpio la memoria
 rm( list=ls() )  #remove all objects
 gc()             #garbage collection
 
 require("data.table")
-require("xgboost")
+require("lightgbm")
 
 #Aqui se debe poner la carpeta de la computadora local
 setwd("/Users/jpc/Documents/00 Data Science Big data Carreer AI/01 Universidad Austral 2022/01_LabImp01/")  #Establezco el Working Directory
@@ -21,16 +21,16 @@ dataset[ , clase01 := ifelse( clase_ternaria=="BAJA+2", 1L, 0L) ]
 campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria","clase01") )
 
 
-#dejo los datos en el formato que necesita XGBoost
-dtrain  <- xgb.DMatrix( data= data.matrix(  dataset[ , campos_buenos, with=FALSE]),
+#dejo los datos en el formato que necesita LightGBM
+dtrain  <- lgb.Dataset( data= data.matrix(  dataset[ , campos_buenos, with=FALSE]),
                         label= dataset$clase01 )
 
 #genero el modelo con los parametros por default
-modelo  <- xgb.train( data= dtrain,
-                      param= list( objective=       "binary:logistic",
-                                   max_depth=           4,
-                                   min_child_weight=   20 ),
-                      nrounds= 40
+modelo  <- lgb.train( data= dtrain,
+                      param= list( objective=        "binary",
+                                   num_iterations=     452,  #40
+                                   num_leaves=         698,  #64
+                                   min_data_in_leaf= 6950 ) #3000
                     )
 
 #aplico el modelo a los datos sin clase
@@ -45,16 +45,26 @@ prediccion  <- predict( modelo,
 entrega  <- as.data.table( list( "numero_de_cliente"= dapply[  , numero_de_cliente],
                                  "Predicted"= prediccion )  ) #genero la salida
 
-
 dir.create( "./labo/",  showWarnings = FALSE ) 
 setwd("/Users/jpc/Documents/00 Data Science Big data Carreer AI/01 Universidad Austral 2022/01_LabImp01/labo/") 
 dir.create( "./exp/", showWarnings = FALSE )
 setwd("/Users/jpc/Documents/00 Data Science Big data Carreer AI/01 Universidad Austral 2022/01_LabImp01/labo/exp/") 
-dir.create( "./KA2552/", showWarnings = FALSE )
-setwd("/Users/jpc/Documents/00 Data Science Big data Carreer AI/01 Universidad Austral 2022/01_LabImp01/labo/exp/KA2552/") 
-archivo_salida  <- "./KA_552_001_probabilidad.csv"
+dir.create( "./KA2512/", showWarnings = FALSE )
+setwd("/Users/jpc/Documents/00 Data Science Big data Carreer AI/01 Universidad Austral 2022/01_LabImp01/labo/exp/KA2512/") 
+archivo_salida  <- "./KA_512_001_default_probabilidad.csv"
+
 
 #genero el archivo para Kaggle
 fwrite( entrega, 
         file= archivo_salida, 
         sep= "," )
+
+
+#ahora imprimo la importancia de variables
+tb_importancia  <-  as.data.table( lgb.importance(modelo) ) 
+archivo_importancia  <- "./512_importancia_001 default.txt"
+
+fwrite( tb_importancia, 
+        file= archivo_importancia, 
+        sep= "\t" )
+
